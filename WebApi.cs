0f3e1959a51163a37b4c59c3c6d5237298de5142
Vilmantas Juras <vilmantas.juras@adform.com>
2019-05-23 00:19:26 +0300

    Cleanup WebApi

--- WebApi.cs
+++ WebApi.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Versioning;
 using Microsoft.Owin.Hosting;
 using Owin;
-using Prometheus.Client;
 using Prometheus.Client.Owin;
 
 namespace PrometheusClientTest
@@ -14,7 +9,6 @@ namespace PrometheusClientTest
     {
         public static IDisposable Start()
         {
-            CreateMetricForApplicationBuildInfo();
             StartOptions startOptions = new StartOptions($"http://*:3000/");
             return WebApp.Start(startOptions, BuildApp);
         }
@@ -26,23 +20,6 @@ namespace PrometheusClientTest
                 q.MapPath = "/metrics";
                 q.UseDefaultCollectors = false;
             });
-        }
-
-        private static void CreateMetricForApplicationBuildInfo()
-        {
-            Assembly executingAssembly = Assembly.GetEntryAssembly();
-
-            string version = FileVersionInfo.GetVersionInfo(executingAssembly.Location).ProductVersion.TrimStart('v');
-            string framework = executingAssembly
-                .GetCustomAttributes(typeof(TargetFrameworkAttribute), false)
-                .OfType<TargetFrameworkAttribute>().FirstOrDefault()?.FrameworkName.ToLowerInvariant();
-
-            Gauge info = Metrics.CreateGauge(
-                "application_build_info",
-                "Application build info",
-                "application", "product_version", "framework_name");
-
-            info.WithLabels(executingAssembly.FullName, version, framework).Set(0);
         }
     }
 }
